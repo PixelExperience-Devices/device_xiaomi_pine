ba074abadf6f0c7629037ce7b3b64f8d76711e86
Bruno Martins <bgcngm@gmail.com>
2019-09-25 23:00:35 +0100

    pine: Kill framework compatibility matrix with fire
    This thing is not mandatory at all and only demands extra work by having
    to keep it up-to-date whenever new interfaces are added to the device manifest
    or simply uprev'ed.
    
    Change-Id: I7c5485f201efde86ff1b277951e7faebf8b80437

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,7 +119,6 @@ TARGET_NO_RPC := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
