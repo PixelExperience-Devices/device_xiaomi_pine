fd94ed4052caddc29b7e13580527e899e62ebae4
GiaSen <giammasena@gmail.com>
2020-09-11 16:16:35 +0200

    pine: copy vndk-29 libbinderthreadstate.so to vendor
    09-11 14:55:24.310  1517  1517 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library "libbinderthreadstate.so" not found: needed by /vendor/lib64/libbindergx.so in namespace (default)

--- device.mk
+++ device.mk
@@ -381,7 +381,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
-# wifi
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbinderthreadstate.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinderthreadstate.so
+
+# Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     libcld80211 \
