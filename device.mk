4c2f1aa0f779a8e1b49aeae673691252e6593b7e
Michael Bestas <mkbestas@lineageos.org>
2020-11-23 20:47:24 +0200

    pine: Don't build textclassifier
    * Target no longer exists
    
    Change-Id: I2079d7dbbeb59bbe9cd925a56f6bcc84c8a38d7f

--- device.mk
+++ device.mk
@@ -358,10 +358,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_HOST_PACKAGES += \
     signapk
 
-# TextClassifier smart selection model files
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
