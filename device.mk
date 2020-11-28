5d7ceff5545706abecc22a4f9f90eaa6b74735c2
Dhina17 <dhinalogu@gmail.com>
2020-11-29 01:31:11 +0530

    pine: power: introduce common AIDL power HAL
    * This commit includes
    
    Author: Arian <arian.kulmer@web.de>
    Date:   2020-09-22 20:06:14 +0200
    
        Build new aidl qti power HAL
    
        Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   2020-10-09 12:19:37 +0100
    
        power: Add extension to new AIDL power HAL
    
        [Dhina17 - Adapted for onclite]
        [TTTT55 - Adapted for pine]
        Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9
    
    Change-Id: I2efcb11c7ff8fcaea0e885a23d0f5f9a954d1018

--- device.mk
+++ device.mk
@@ -123,6 +123,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/permissions/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
