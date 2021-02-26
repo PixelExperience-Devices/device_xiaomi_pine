b32fad945221eb12629f498db13216835f4f2250
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-02-26 21:58:56 +0530

    pine: import stock manifest
    with minimal changes for Android 11

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,9 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
