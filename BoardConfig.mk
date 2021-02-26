9edbc79835c29a5f6bb5aacfe2eb5f546d49fe5d
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-02-26 20:14:30 +0530

    pine: add kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,3 +30,14 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8937
 TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE :=  2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_SOURCE := kernel/xiaomi/pine
+TARGET_KERNEL_CONFIG := pine-perf_defconfig
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_VERSION := 4.9
