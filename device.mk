d226145b0e64a7b43c7091f1a5dd183588736446
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-02-27 23:33:48 +0530

    pine: build WiFi display packages

--- device.mk
+++ device.mk
@@ -397,6 +397,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wi-Fi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # XiaomiDoze
 PRODUCT_PACKAGES += \
     XiaomiDoze
