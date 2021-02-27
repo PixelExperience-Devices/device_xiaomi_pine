dccff6964cca3f98b069dc65d7f50f148e44e012
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-02-27 23:31:36 +0530

    pine: compile Vibrator blobs from source

--- device.mk
+++ device.mk
@@ -361,6 +361,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Whitelisted apps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
