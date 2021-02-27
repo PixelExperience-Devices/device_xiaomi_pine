c78ce2aaa59b0dd6275013544989e7db7f35aaaf
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-02-27 23:24:04 +0530

    pine: build libjson

--- device.mk
+++ device.mk
@@ -284,6 +284,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
