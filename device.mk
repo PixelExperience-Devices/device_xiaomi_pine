f6ac3e381194bff59089a7173f95a2e95104bd5b
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-02-27 07:35:17 +0530

    pine: inherit vendor blobs

--- device.mk
+++ device.mk
@@ -6,3 +6,5 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/xiaomi/pine/pine-vendor.mk)
