fec26605f3e54ef0a2d9a47791a1855ca84f516e
Alexander <yaalex@yaalex.tk>
2021-03-31 07:23:46 +0000

    pine: Fluid
    Signed-off-by: Alexander <yaalex@yaalex.tk>

--- fluid_pine.mk
+++ fluid_pine.mk
@@ -12,16 +12,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/pine/device.mk)
 
 # Inherit some LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 720
 
-# Device Maintainer
-DEVICE_MAINTAINER := AOiSPdev
-
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_pine
+PRODUCT_NAME := fluid_pine
 PRODUCT_DEVICE := pine
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7A
@@ -33,6 +30,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="pine-user 10 QKQ1.191014.001 V12.0.1.0.QCMMIXM release-keys"
+
+# Fluid flags
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.fluid.maintainer=YaAlex \
+	ro.fluid.cpu=SDM439
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/pine/pine:10/QKQ1.191014.001/V12.0.1.0.QCMMIXM:user/release-keys
